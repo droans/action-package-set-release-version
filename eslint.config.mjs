529f176de89ded1145e6ab4ff276fee7f41590fa
Michael Carroll <mjoecarroll@gmail.com>
2025-12-29 11:50:18 -0500

    Add compilation, lint files

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import lit from "eslint-plugin-lit";
+import wc from 'eslint-plugin-wc';
+const rootConfigFiles = [".prettierrc.js", "eslint.config.mjs"];
+
+export default tseslint.config(
+  { ignores: ["dist", "node_modules/*"] },
+  // apply default config
+  prettierConfig,
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    plugins: {
+      lit: lit,
+      wc: wc,
+    },
+    rules: {
+      ...lit.configs.recommended.rules,
+      ...wc.configs.recommended.rules,
+    }
+  },
+  // default language/parser options
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: rootConfigFiles,
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  // individual rule overrides
+  {
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/no-unnecessary-condition": "off",
+      "@typescript-eslint/non-nullable-type-assertion-style": "off"
+    },
+  },
+  // disable type checking for root config files
+  {
+    files: rootConfigFiles,
+    ...tseslint.configs.disableTypeChecked,
+    languageOptions: { sourceType: "commonjs" },
+    rules: {
+      ...tseslint.configs.disableTypeChecked.rules,
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  }
+);
---
+++ rollup.config.dev.js
@@ -0,0 +1,23 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from 'rollup-plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: ['src/main.ts'],
+  output: {
+    file: '../dist/index.ts',
+    format: 'es',
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript(),
+    json(),
+    terser(),
+  ],
+  onwarn(warning, warn) {
+    warn(warning);
+  },
+};
